82db4b0cb33eed6f7bdd7f1eeb2920754827ea1b
Yufeireal <yufei.zheng@verkada.com>
2025-05-04 22:07:26 -0700

    feat: add base64 encode/decode

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,14 +1,15 @@
 use std::path::Path;
 
+pub use base64::Base64SubCommand;
 use clap::Parser;
 use csv::CsvOpts;
 use genpass::GenPassOpts;
-
 mod base64;
 mod csv;
 mod genpass;
 
 pub use self::csv::OutputFormat;
+pub use base64::Base64Format;
 
 #[derive(Debug, Parser)]
 #[command(name="rcli", version, author, about, long_about = None)]
@@ -23,6 +24,8 @@ pub enum SubCommand {
     Csv(CsvOpts),
     #[command(name = "genpass", about = "Generate a random password")]
     GenPass(GenPassOpts),
+    #[command(subcommand)]
+    Base64(Base64SubCommand),
 }
 
 fn verify_input_file(filename: &str) -> Result<String, &'static str> {
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 mod cli;
 mod process;
 
-pub use cli::{Opts, SubCommand};
+pub use cli::{Base64Format, Base64SubCommand, Opts, SubCommand};
 
 pub use process::*;
